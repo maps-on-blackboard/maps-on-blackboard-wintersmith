d5b71a002f3654856680465f29990281f7deec3c
Gagan Bansal <gaganbansal123@gmail.com>
2015-05-31 08:11:00 -0400

    create blog script modified

--- create-blog-repo.sh
+++ create-blog-repo.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-echo "source blog: "
-read source
-echo "new blog name: "
-read dest
+read -p "source blog: " -i blog2-thematic-map -e source
+read -p "new blog name: " -e dest
 mkdir $dest
 rsync -av \
   --exclude='**/node_modules' \
@@ -17,6 +15,9 @@ echo "created: $dest"
 
 echo "creating git repo '$dest' in maps-on-blackboard .."
 
+read -p "Description: " -i 'This repository is part of my blog maps-on-blackboard' -e desc
+read -p "Homepage along description: " -i http://maps-on-blackboard.com -e homepage
+#  "http://maps-on-blackboard.com" homepage
 #read -p "Description for this repo: " -e -i \
 #  "This repository is part of my blog maps-on-blackboard" desc
 #read -p "Homepage along description: " -e -i \
@@ -24,13 +25,19 @@ echo "creating git repo '$dest' in maps-on-blackboard .."
 
 git init;
 echo "node_modules" > .gitignore
+data='{ "name":"'$dest'", "description": "'$desc'", "homepage": "'$homepage'" }'
+echo $data > /home/gaganb/tmp/data.txt
+read -p "want to proceed" -e ans
+curl -o /home/gaganb/create-project.log -u 'gaganbansal123@gmail.com' \
+  https://api.github.com/orgs/maps-on-blackboard/repos \
+  -d @/home/gaganb/tmp/data.txt
 
 #curl -s -u 'gaganbansal123@gmail.com' \
 #  https://api.github.com/orgs/maps-on-blackboard/repos \
 #  -d '{ "name":"'$dest'", "description": "This repository is part of my blog maps-on-blackboard.", "homepage": "http://maps-onblackboard.com/" }'
 
 repo=$dest'.git'
-#git remote add origin https://github.com/maps-on-blackboard/$repo
+git remote add origin https://github.com/maps-on-blackboard/$repo
 
 echo "Installing npm modules .."
 npm install
