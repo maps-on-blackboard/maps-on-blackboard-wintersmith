edd6acb33e1d71391b5101901f813f087f3cfd30
Gagan Bansal <gaganbansal123@gmail.com>
2015-05-31 08:20:10 -0400

    add subtree script modified

--- add-subtree-blog-repo.sh
+++ add-subtree-blog-repo.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-echo "blog repo: "
-read repo
+read -p "blog repo: " -e repo
 git remote add $repo https://github.com/maps-on-blackboard/$repo
 git subtree add --prefix=contents/articles/$repo --squash $repo master
 
 cd contents/articles/$repo
-npm install
+#npm install
 
